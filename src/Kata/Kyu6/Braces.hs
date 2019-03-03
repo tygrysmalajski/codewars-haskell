644724ca3f404a6598076cf545a64f64df744f67
malayantiger <jurny.samuraj@gmail.com>
2019-03-03 09:28:10 +0100

    Fix bug in 'Valida braces' kata

--- src/Kata/Kyu6/Braces.hs
+++ src/Kata/Kyu6/Braces.hs
@@ -1,21 +1,27 @@
 -- https://www.codewars.com/kata/valid-braces
 
-module Kata.Kyu6.Braces where
+module Kata.Kyu6.Braces (validBraces) where
+import Data.List
 
 validBraces :: String -> Bool
-validBraces xs = foldl process (0, 0, 0) xs == (0, 0, 0)
+validBraces = process []
     where
-        process (c, b, p) x
-            | c < 0 || b < 0 || p < 0 = (c, b, p)
-            | x == '}' = curly dec
-            | x == '{' = curly inc
-            | x == '[' = bracket inc
-            | x == ']' = bracket dec
-            | x == '(' = paranthese inc
-            | x == ')' = paranthese dec
-            where
-                curly f = (f c, b, p)
-                bracket f = (c, f b, p)
-                paranthese f = (c, b, f p)
-                inc x' = x' + 1
-                dec x' = x' - 1
+        process [] [] = True
+        process _ [] = False
+        process [] (x:xs)
+            | open x = process (x:[]) xs
+            | otherwise = False
+        process (c:opened) (x:xs)
+            | open x = process (x:c:opened) xs
+            | x `closes` c = process opened xs
+            | otherwise = False
+
+open :: Char -> Bool           
+open x = elem x "{[("
+
+closes :: Char -> Char -> Bool
+closes x y
+    | y == '{' && x == '}' = True
+    | y == '[' && x == ']' = True
+    | y == '(' && x == ')' = True
+    | otherwise = False
